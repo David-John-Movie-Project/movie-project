2eafc0d5bee66e43aa9a10a6f416284fe14ba2a3
John_Pedrotti <jdpedrotti@gmail.com>
2022-10-05 11:57:13 -0500

    working on form submit

--- movie-project.js
+++ movie-project.js
@@ -65,3 +65,34 @@ const deleteOptions = {
 
 // use themoviedb.org/documentation/api   to get the movies
 
+
+$('#form1').submit((e) => {
+    e.preventDefault();
+
+    let addMovie = {
+        title: $("#title").val(),
+        genre: $("#genre").val(),
+        rating: $("#rating").val(),
+        description: $("#description").val(),
+    }
+    console.log("this is the add movie log")
+    console.log(addMovie)
+
+    let postOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(addMovie)
+    }
+    //POST movie
+function getMovies() {
+    fetch(moviesURL, postOptions)
+        .then(resp => resp.json().then(data=>console.log(data))
+        .then(moviePosters => {
+            console.log(moviePosters)
+        }).catch(error => console.log(error)))
+}
+});
+
+getMovies()
