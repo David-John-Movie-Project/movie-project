46cc72ea804988318c7483e60e57e9560fd29139
David Lara <david.a.lara05@gmail.com>
2022-10-06 11:59:00 -0500

    fixed hover and cards

--- movie-project.js
+++ movie-project.js
@@ -1,46 +1,46 @@
-    const movieAPI = "MOVIE_PROJECT_KEY"
+const movieAPI = "MOVIE_PROJECT_KEY"
 
 
-    // This gets the movies from our glitch
-    const moviesURL = "https://tender-brick-bowler.glitch.me/movies"
+// This gets the movies from our glitch
+const moviesURL = "https://tender-brick-bowler.glitch.me/movies"
 
-    // function getMovies() {
-    //     fetch(moviesURL)
-    //         .then(resp => resp.json()).then(data=>console.log(data));
-    // }
-    //
-    // getMovies();
+// function getMovies() {
+//     fetch(moviesURL)
+//         .then(resp => resp.json()).then(data=>console.log(data));
+// }
+//
+// getMovies();
 
 
 
-                            // this lets you add a movie function
+// this lets you add a movie function
 
-    const movieToPost = {
-        title: "Eleanor of Aquitaine",
-        author: {
-            firstName: "Ralph",
-            lastName: "Turner"
-        }
+const movieToPost = {
+    title: "Eleanor of Aquitaine",
+    author: {
+        firstName: "Ralph",
+        lastName: "Turner"
     }
+}
 
-    const postOptions = {
-        method: 'POST',
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-        body: JSON.stringify(movieToPost)
-    }
+const postOptions = {
+    method: 'POST',
+    headers: {
+        'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify(movieToPost)
+}
 
-    // function getMovies() {
-    //     fetch(moviesURL).then(resp => resp.json()).then(data=>console.log(data));
-    // }
+// function getMovies() {
+//     fetch(moviesURL).then(resp => resp.json()).then(data=>console.log(data));
+// }
 
-    // getMovies();
+// getMovies();
 
-    // fetch(moviesURL, postOptions).then(getMovies);
+// fetch(moviesURL, postOptions).then(getMovies);
 
 
-                        // This deletes the movies
+// This deletes the movies
 
 async function deleteCard(id) {
     const deleteOptions = {
@@ -60,8 +60,7 @@ async function deleteCard(id) {
 
 
 
-
-                    // This adds the movie into the array with the form
+// This adds the movie into the array with the form
 $('#form1').submit((e) => {
     e.preventDefault();
 
@@ -96,7 +95,7 @@ $('#form1').submit((e) => {
 
 
 
-                   // This creates the cards when we add a movie
+// This creates the cards when we add a movie
 
 async function movieGlitch() {
     try {
@@ -107,9 +106,9 @@ async function movieGlitch() {
         let moviesHTML = '';
         events.forEach((movie, index) => {
             moviesHTML += `
-            <div class="card ml-4 " style="height: 500px; width: 100%; background-color: #5d5d5d; color: white">
+            <div class="card ml-4" style="border-radius:  0;  height: 500px; width: 100%; background-color: #5d5d5d; color: white">
             
-                <div class="card-img-top"> ${movie.poster ? `<img class="poster mb-4" src="${movie.poster}" />` : ''}</div> 
+                <div class="card-img-top"> ${movie.poster ? `<img style="width:216px; height:500px" class="poster" src="${movie.poster}" />` : ''}</div> 
                 <div class="banner-content">
                 <h3>${movie.title ?? ''}</h3>
                 <p>${movie.genre ?? ''}</p>
@@ -118,8 +117,7 @@ async function movieGlitch() {
 
                 <br>
                 <button class="deleteMovieCard" data-delete-card="${movie.id}">Delete</button>
-                <button class="editMovieCard"
-                data-edit-card=${movie.id}">Edit<button>
+                <button class="editMovieCard" data-edit-card=${movie.id}">Edit<button>
        </div>
                             </div>
 
@@ -132,43 +130,36 @@ async function movieGlitch() {
     }
 }
 
-    movieGlitch();
+movieGlitch();
 
 
-    function getMovies() {
-        fetch(moviesURL).then(resp => resp.json()).then(data=>console.log(data));
-    }
-    getMovies()
-
-
-
-            // Add event listener to delete the card and edit
-    $(document.body).on("click", ".deleteMovieCard", function(){
-        // console.log($(this).attr("data-delete-card"))
-        deleteCard($(this).attr("data-delete-card"))
-    });
-
-    // $(document.body).on("click", "editMovieCard", function (){
-
-    // }  )
+function getMovies() {
+    fetch(moviesURL).then(resp => resp.json()).then(data=>console.log(data));
+}
+getMovies()
 
 
 
+// Add event listener to delete the card
+$(document.body).on("click", ".deleteMovieCard", function(){
+    // console.log($(this).attr("data-delete-card"))
+    deleteCard($(this).attr("data-delete-card"))
+});
+
+//      adjusts playback speed of video
+document.querySelector('video').playbackRate = .6;
 
 
 
-                         //      adjusts playback speed of video
-    document.querySelector('video').playbackRate = .6;
+//    side bar
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
 
-                            //    side bar
-    /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-    function openNav() {
-        document.getElementById("mySidebar").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
-    }
-
-    /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-    function closeNav() {
-        document.getElementById("mySidebar").style.width = "0";
-        document.getElementById("main").style.marginLeft = "0";
-    }
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
