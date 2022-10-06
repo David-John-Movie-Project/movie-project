97652030f069b51a15ef26945c81d5f2bafae2be
David Lara <david.a.lara05@gmail.com>
2022-10-06 10:37:21 -0500

    added sidebar

--- movie-project.js
+++ movie-project.js
@@ -150,3 +150,17 @@ async function movieGlitch() {
     //      adjusts playback speed of video
     document.querySelector('video').playbackRate = .6;
 
+
+
+//    side bar
+    /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+    function openNav() {
+        document.getElementById("mySidebar").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+    }
+
+    /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+    function closeNav() {
+        document.getElementById("mySidebar").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+    }
